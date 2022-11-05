bd49bbcfcc82aa2f0f59b22985b2bc05f46cb0ca
ibrahim <ebrahimaboeita990@gmail.com>
2022-11-05 02:54:18 +0200

    setting the file tree and installing diagnosing requirements

--- City/hospital/urls.py
+++ City/hospital/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name = 'home')
+    path('', views.hospital, name = 'hospital'), 
+    path('braintumor/', views.braintumor, name = 'braintumor'),
+    path('breastcancer/', views.breastcancer, name = 'breastcancer')
 ]
--- City/hospital/views.py
+++ City/hospital/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'hospital.html')
+def hospital(request):
+    title = 'Diagnosing'
+    return render(request, 'hospital.html', {'title': title})
+
+
+#--------------------------------------------------------------------------------------------
+#Diagnosing Functionality
+
+
+
+
+#--------------------------------------------------------------------------------------------
+def braintumor(request):
+    title = 'Brain Tumor'
+    return render(request, 'braintumor.html', {'title': title})
+
+def breastcancer(request):
+    title = 'Brast Cancer'
+    return render(request, 'breastcancer.html', {'title': title})
+
+
+
