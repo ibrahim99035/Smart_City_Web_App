6e42eafd5cec05ac9e33ce59b703cf119b837526
ibrahim <ebrahimaboeita990@gmail.com>
2022-12-09 04:23:51 +0200

    team data mechanism

---
+++ City/base/team.py
@@ -0,0 +1,85 @@
+class Team:
+    team = {
+        1 : {
+            "name" : "name 1",
+            "phone" : "phone 1",
+            "email" : "email 1",
+            "github" : "link 1",
+            "linkedin" : "link 1",
+            "photo" : "path 1"
+        },
+        2 : {
+            "name" : "name 2",
+            "phone" : "phone 2",
+            "email" : "email 2",
+            "github" : "link 2",
+            "linkedin" : "link 2",
+            "photo" : "path 2"
+        },
+        3 : {
+            "name" : "name 3",
+            "phone" : "phone 3",
+            "email" : "email 3",
+            "github" : "link 3",
+            "linkedin" : "link 3",
+            "photo" : "path 3"
+        },
+        4 : {
+            "name" : "name 4",
+            "phone" : "phone 4",
+            "email" : "email 4",
+            "github" : "link 4",
+            "linkedin" : "link 4",
+            "photo" : "path 4"
+        },
+        5 : {
+            "name" : "name 5",
+            "phone" : "phone 5",
+            "email" : "email 5",
+            "github" : "link 5",
+            "linkedin" : "link 5",
+            "photo" : "path 5"
+        },
+        6 : {
+            "name" : "name 6",
+            "phone" : "phone 6",
+            "email" : "email 6",
+            "github" : "link 6",
+            "linkedin" : "link 6",
+            "photo" : "path 6"
+        },
+        7 : {
+            "name" : "name 7",
+            "phone" : "phone 7",
+            "email" : "email 7",
+            "github" : "link 7",
+            "linkedin" : "link 7",
+            "photo" : "path 7"
+        },
+        8 : {
+            "name" : "name 8",
+            "phone" : "phone 8",
+            "email" : "email 8",
+            "github" : "link 8",
+            "linkedin" : "link 8",
+            "photo" : "path 8"
+        },
+        9 : {
+            "name" : "name 9",
+            "phone" : "phone 9",
+            "email" : "email 9",
+            "github" : "link 9",
+            "linkedin" : "link 9",
+            "photo" : "path 9"
+        },
+        10 : {
+            "name" : "name 10",
+            "phone" : "phone 10",
+            "email" : "email 10",
+            "github" : "link 10",
+            "linkedin" : "link 10",
+            "photo" : "path 10"
+        },
+    }
+    def getTeamData(self):
+        return self.team 
--- City/base/views.py
+++ City/base/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
-
+from base.team import Team
 def home(request):
     title = 'Home'
     return render(request, 'home.html', {'title': title})
+
+def contact(request):
+    title = 'Contact Us'
+    return render(request, 'contact.html', {'title': title})
+
+def about(request):
+    title = 'About Us'
+    teamObj = Team()
+    teamData = teamObj.getTeamData()
+    key1 = [1,2,3,4,5,6,7,8,9,10]
+    key2 = [0,1,2,3,4,5]
+    return render(request, 'about.html', {'title': title, 'team': teamData, 'key1': key1, 'key2': key2})
