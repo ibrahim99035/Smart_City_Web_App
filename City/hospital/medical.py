29b0265d81ecab3ee668b91293a933dfec836711
ibrahim <ebrahimaboeita990@gmail.com>
2023-06-30 08:25:05 +0300

    tests

--- City/hospital/medical.py
+++ City/hospital/medical.py
@@ -13,7 +13,7 @@ class Anemia:
         elif test3 or test4:
             self.AnemiaResult = self.positiveResult
         else:
-            self.AnemiaResult = self.positiveResult#'invalid input'
+            self.AnemiaResult = 'invalid input'
 
         return self.AnemiaResult
     
@@ -27,10 +27,10 @@ class Diabetes:
 
     def checkTheCase(self, Fasting, After_Eating, Hours_After_Eating):
         #diabetes tests boolean variables
-        test1 = 80.0 <= Fasting.data <= 100.0 and 170.0 <= After_Eating.data <= 200.0 and 120.0 <= Hours_After_Eating.data <= 140.0
-        test2 = 101.0 <= Fasting.data <= 125.0 and 190.0 <= After_Eating.data <= 230.0 and 140.0 <= Hours_After_Eating.data <= 160.0
-        test3 = Fasting.data >=126.0 and 220.0 <= After_Eating.data <= 300.0 and  Hours_After_Eating.data >= 200.0
-        test4 = Fasting.data <80.0
+        test1 = 80.0 <= Fasting <= 100.0 and 170.0 <= After_Eating <= 200.0 and 120.0 <= Hours_After_Eating <= 140.0
+        test2 = 101.0 <= Fasting <= 125.0 and 190.0 <= After_Eating <= 230.0 and 140.0 <= Hours_After_Eating <= 160.0
+        test3 = Fasting >=126.0 and 220.0 <= After_Eating <= 300.0 and  Hours_After_Eating >= 200.0
+        test4 = Fasting <80.0
 
         #the functionality 
         if test1:
@@ -54,31 +54,31 @@ class Kidney:
     KidneyResult = ''
 
     def checkKidney(self, Creatinin, Creatinin_Clearance, Na, K, Cl, Blood_Urine_Nitrogen, Urea):
-        CreatininHealthy = Creatinin.data >= 0.7 and Creatinin.data <= 1.4
-        Creatinin_ClearanceHealthy = Creatinin_Clearance.data >= 97.0 and Creatinin_Clearance.data <= 137.0
-        NaHealthy = Na.data >= 135.0 and Na.data <= 148.0
-        KHealthy = K.data >= 3.5 and K.data <= 5.0
-        ClHealthy = Cl.data >= 95.0 and Cl.data <= 105.0
-        Blood_Urine_NitrogenHealthy = Blood_Urine_Nitrogen.data >= 7.0 and Blood_Urine_Nitrogen.data <= 20.0
-        UreaHealthy = Urea.data >= 20.0 and Urea.data <= 40.0
+        CreatininHealthy = Creatinin >= 0.7 and Creatinin <= 1.4
+        Creatinin_ClearanceHealthy = Creatinin_Clearance >= 97.0 and Creatinin_Clearance <= 137.0
+        NaHealthy = Na >= 135.0 and Na <= 148.0
+        KHealthy = K >= 3.5 and K <= 5.0
+        ClHealthy = Cl >= 95.0 and Cl <= 105.0
+        Blood_Urine_NitrogenHealthy = Blood_Urine_Nitrogen >= 7.0 and Blood_Urine_Nitrogen <= 20.0
+        UreaHealthy = Urea >= 20.0 and Urea <= 40.0
         TestHealthy = CreatininHealthy and Creatinin_ClearanceHealthy and NaHealthy and KHealthy and ClHealthy and Blood_Urine_NitrogenHealthy and UreaHealthy
         #----------------------------------------------------------------------------------------------------
-        CreatininTired = Creatinin.data >= 1.5  
-        Creatinin_ClearanceTired = Creatinin_Clearance.data <=95.0
-        NaTired = Na.data >= 135.0 and Na.data <= 148.0
-        KTired = K.data >3.5 
-        ClTired = Cl.data >= 95.0 and Cl.data <= 105.0
-        Blood_Urine_NitrogenTired = Blood_Urine_Nitrogen.data >= 7.0 and Blood_Urine_Nitrogen.data <= 20.0
-        UreaTired = Urea.data >= 40.0 and Urea.data <= 200.0
+        CreatininTired = Creatinin >= 1.5  
+        Creatinin_ClearanceTired = Creatinin_Clearance <=95.0
+        NaTired = Na >= 135.0 and Na <= 148.0
+        KTired = K >3.5 
+        ClTired = Cl >= 95.0 and Cl <= 105.0
+        Blood_Urine_NitrogenTired = Blood_Urine_Nitrogen >= 7.0 and Blood_Urine_Nitrogen <= 20.0
+        UreaTired = Urea >= 40.0 and Urea <= 200.0
         TestTired = CreatininTired and Creatinin_ClearanceTired and NaTired and KTired and ClTired and Blood_Urine_NitrogenTired and UreaTired
         #----------------------------------------------------------------------------------------------------
-        CreatininDialysis = Creatinin.data >7.0
-        Creatinin_ClearanceDialysis = Creatinin_Clearance.data <= 15.0 
-        NaDialysis = Na.data >= 135.0 and Na.data <= 148.0
-        KDialysis = K.data >5.5
-        ClDialysis = Cl.data >= 95.0 and Cl.data <= 105.0
-        Blood_Urine_NitrogenDialysis = Blood_Urine_Nitrogen.data >= 7.0 and Blood_Urine_Nitrogen.data <= 20.0
-        UreaDialysis = Urea.data >200.0
+        CreatininDialysis = Creatinin >7.0
+        Creatinin_ClearanceDialysis = Creatinin_Clearance <= 15.0 
+        NaDialysis = Na >= 135.0 and Na <= 148.0
+        KDialysis = K >5.5
+        ClDialysis = Cl >= 95.0 and Cl <= 105.0
+        Blood_Urine_NitrogenDialysis = Blood_Urine_Nitrogen >= 7.0 and Blood_Urine_Nitrogen <= 20.0
+        UreaDialysis = Urea >200.0
         TestDialysis = CreatininDialysis and Creatinin_ClearanceDialysis and NaDialysis and KDialysis and ClDialysis and Blood_Urine_NitrogenDialysis and UreaDialysis
         #----------------------------------------------------------------------------------------------------
         if TestHealthy:
@@ -93,14 +93,14 @@ class Kidney:
         return KidneyResult
 
 class SurigcalOperation:
-    test1  = '(Hemoglopen)'
-    test2  = '(White Blood)'
-    test3  = '(Platelets)'
-    test4  = '(Liver)'
-    test5  = '(Kidney)'
-    test6  = '(Fluidity)'
+    test1  = 'Hemoglopen'
+    test2  = 'White Blood'
+    test3  = 'Platelets'
+    test4  = 'Liver'
+    test5  = 'Kidney'
+    test6  = 'Fluidity'
     #strings
-    safe = '(None)'
+    safe = 'None'
     message = ', The patient has a problem with '
     splited =''
     patientResult = ''
@@ -110,8 +110,8 @@ class SurigcalOperation:
     
     #checking patient's results
     def checkpatientResult(self, hemoglopen, whiteBlood, platelets, liver, kidney, fluidity):
-        if 9 <= hemoglopen.data <= 11 and 5000.0 <= whiteBlood.data <= 18000.0 and 150000 <= platelets.data <= 350000 and \
-        20 <= liver.data <= 40 and 0.5 <= kidney.data <= 1.5 and 0.7 <= fluidity.data <= 1.5:
+        if 9 <= hemoglopen <= 11 and 5000.0 <= whiteBlood<= 18000.0 and 150000 <= platelets <= 350000 and \
+        20 <= liver <= 40 and 0.5 <= kidney<= 1.5 and 0.7 <= fluidity <= 1.5:
             patientResult = self.qualified
         else:
             patientResult = self.unqualified
